ebfd746da949581eaa513a2e25bf0d1ca78ff2c7
Alex <akrumm1124@gmail.com>
2021-03-18 15:13:51 -0500

    First commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex
+ */
+public class Employee {
+   private String name;
+   private int idNumber;
+   private String department;
+   private String position;
+   
+   public Employee() {
+       name = "";
+       idNumber = 0;
+       department = "";
+       position = "";
+   }
+   
+   public Employee(String employeeName, int id, String dpt, String pstn) {
+       name = employeeName;
+       idNumber = id;
+       department = dpt;
+       position = pstn;
+   }
+   
+   public Employee(String empName, int empID) {
+       name = empName;
+       idNumber = empID;
+       department = "";
+       position = "";
+   }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex
+ */
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee ("Susan Meyers", 47899, "Accounting", "Vice President");
+        Employee employee2 = new Employee ("Mark Jones", 39119, "IT", "Programmer");
+        Employee employee3 = new Employee ("Joy Rogers", 81774, "Manufacturing", "Engineer");
+        
+        System.out.println("Name: " +employee1.getName() + "; ID num: " + employee1.getIdNumber() + "; Department: " + employee1.getDepartment() + "; Position: " + employee1.getPosition());
+        System.out.println("Name: " +employee2.getName() + "; ID num: " + employee2.getIdNumber() + "; Department: " + employee2.getDepartment() + "; Position: " + employee2.getPosition());
+        System.out.println("Name: " +employee3.getName() + "; ID num: " + employee3.getIdNumber() + "; Department: " + employee3.getDepartment() + "; Position: " + employee3.getPosition());
+    }
+}
